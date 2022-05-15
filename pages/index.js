ed9150d48168b81b1fbd8e741af6180801aae639
Andrew Sia <andrewsia8312@gmail.com>
2022-05-14 18:43:25 -0700

    no message

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import AboutSection from '../components/AboutSection/AboutSection'
 import PartnersSection from '../components/PartnersSection/PartnersSection'
 import CollectionsSection from '../components/CollectionsSection/CollectionsSection'
 import SubscriptionsSection from '../components/SubscriptionsSection/SubscriptionsSection'
-import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -13,20 +12,6 @@ export default function Home() {
       <Head>
         <title>AJS Landing Page Project 1</title>
         <meta name="project" content="Landing Page Project" />
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-6QZQ32GWGF"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-6QZQ32GWGF');
-          `}
-        </Script>
       </Head>
       <HeroSection />
       <AboutSection />
